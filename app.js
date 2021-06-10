c5f314149b84fcd58459b04d02d238ab9c1835c9
S460 <aleksandar.ta6ev@gmail.com>
2021-06-10 13:00:05 +0300

    Added template for the messages

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
 app.get('/', (req, res) => {
-	res.render('index', { title: 'Homepage' });
+	res.render('index', { title: 'Homepage', messages });
 });
 
 app.listen(PORT, () => {
