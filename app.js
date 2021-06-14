0d872707ac9cfefa61d3b3654e0e35eb9a65467c
S460 <aleksandar.ta6ev@gmail.com>
2021-06-14 18:53:37 +0300

    Added mongo support

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const express = require('express');
+/* eslint-disable no-unused-vars */
+
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Message = require('./models/Message');
 
 const app = express();
+
+mongoose
+	.connect(process.env.DB_PASS, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Connected to DB.');
+		app.listen(PORT, () => {
+			console.log('Listening on PORT ' + PORT);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 8000;
@@ -11,20 +31,8 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-const messages = [
-	{
-		text: 'Hi there!',
-		user: 'Amando',
-		added: new Date(),
-	},
-	{
-		text: 'Hello World!',
-		user: 'Charles',
-		added: new Date(),
-	},
-];
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+	const messages = await Message.find();
 	res.render('index', { title: 'Message Board', messages });
 });
 
@@ -34,14 +42,12 @@ app.get('/new', (req, res) => {
 
 app.post('/new', (req, res) => {
 	const { text, user } = req.body;
-	messages.push({ text, user, added: new Date() });
-	res.redirect('/');
+	const newMessage = new Message({ text, user, added: new Date() });
+	newMessage.save().then((result) => {
+		res.redirect('/');
+	});
 });
 
 app.use((req, res) => {
 	res.render('404', { title: '404' });
-});
-
-app.listen(PORT, () => {
-	console.log('Listening on PORT ' + PORT);
 });
---
+++ models/Message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+	text: { type: String, required: true },
+	user: { type: String, required: true },
+	added: { type: Date, required: true },
+});
+
+const Message = mongoose.model('Message', MessageSchema);
+
+module.exports = Message;
